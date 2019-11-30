2da8f6dc9756b85e3c8ef8b8e6cec9c6db03f439
Ariana Gonçalves <arygoncalves15@gmail.com>
2019-11-30 22:33:57 +0000

    Creates shingle file from any given book.txt

--- Shingle.java
+++ Shingle.java
@@ -1,18 +1,54 @@
 package mpei;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Shingle {
-	private static int ktitle = 5;
+	private static int ktitle = 4;
 	private static int kcontent = 10;
 	
 	
-	public static String[] createShingleTitle(String titulo){
+	public static void createShingleFile(String path){
+		//Analisar o ficheiro, leitura 
+		File fx = new File(path);
+		String title = "";
+		String content = "";
+		StringBuilder sb = new StringBuilder();
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(path));
+			String line;
+	        while ((line = in.readLine()) != null) {
+	            if (line.startsWith("Title: ")) {
+	                title = removeLinePrefix(line, "Title: ");   
+		            line = in.readLine();
+	            }
+	            if(line.startsWith("*** START OF THIS PROJECT GUTENBERG EBOOK")) {
+	            	while(!((line = in.readLine()).startsWith("*** END OF THIS PROJECT GUTENBERG EBOOK"))) {
+	            		sb.append(line);
+	            		line = in.readLine();
+	            	}
+	            }
+	        }
+	        content = sb.toString();
+	        String[] s_title = createShingleTitle(title);
+	        String[] s_content = createShingleContent(content);
+	        createShingleFile(title, s_title, s_content);
+		} 
+		catch(IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	private static String removeLinePrefix(String line, String prefix) {
+	    return line.substring(prefix.length());
+	}
+			
+	private static String[] createShingleTitle(String titulo) {
 		String nospaces = titulo.replaceAll("\\s+|\\.|\\,|\\;|\\:", "");
 		List<String> shingles = new ArrayList<>();
 		for(int j=0; j<nospaces.length()-ktitle+1; j++) {
@@ -20,13 +56,13 @@ public class Shingle {
 			word = nospaces.substring(j, j+ktitle).toLowerCase();
 			if(!shingles.contains(word)) {
 				shingles.add(word);
-				System.out.print(word + " ");
+				//System.out.print(word + " ");
 			}
 		}
 		return shingles.toArray(new String[0]);
 	}
 	
-	public static String[] createShingleContent(String content){
+	private static String[] createShingleContent(String content){
 		String nospaces = content.replaceAll("\\s+|\\.|\\,|\\;|\\:", "");
 		List<String> shingles = new ArrayList<>();
 		for(int j=0; j<nospaces.length()-kcontent+1; j++) {
@@ -34,62 +70,26 @@ public class Shingle {
 			word = nospaces.substring(j, j+kcontent).toLowerCase();
 			if(!shingles.contains(word)) {
 				shingles.add(word);
-				System.out.print(word + " ");
+				//System.out.println(word + " ");
 			}
 		}
 		return shingles.toArray(new String[0]);
 	}
 	
-	public static boolean writeShingleFile(String path) throws IOException {
-		File shingles = new File("shingles.txt");
-		String title = "";
-		String content = "";
-		shingles.createNewFile();
-		Scanner readfx;
-		try {
-			readfx = new Scanner(new File(path));
-			while(readfx.hasNextLine()) {
-				String line = readfx.nextLine();
-				if(line.startsWith("Title: ")) {
-					title = line.substring(7);
-				}
-				if(line.startsWith("*** START OF THIS PROJECT GUTENBERG EBOOK")) {
-					while(readfx.hasNextLine()) {
-						content = content + " " + readfx.nextLine();
-					}
-				}
-				readfx.close();
-			}
-		}catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		createShingleContent(content);
-		createShingleTitle(title);
-		return true;	
+	private static void createShingleFile(String title, String[] titleset, String[] contentset) throws IOException{
+		File fx = new File("C:\\Users\\Ahri Gonçalves\\eclipse-workspace\\p3\\src\\mpei\\" + title + "_shingles.txt"); 
+		PrintWriter pw = new PrintWriter(fx);
+		for(String i: titleset)
+			pw.print(i + " ");
+		pw.println("");
+		pw.println("");
+		for(String i: contentset)
+			pw.print(i + " ");
+		pw.close();
+		System.out.print("SHINGLE FILE FOR "+title+" BOOK CREATED!");
 	}
-
+	
 	public static void main(String args[]) throws IOException {
-		Shingle.createShingleTitle("Alabama");
-		Shingle.createShingleContent("Amor é fogo que arde sem se ver\r\n" + 
-				"\r\n" + 
-				"Amor é fogo que arde sem se ver;\r\n" + 
-				"É ferida que dói e não se sente;\r\n" + 
-				"É um contentamento descontente;\r\n" + 
-				"É dor que desatina sem doer;\r\n" + 
-				"\r\n" + 
-				"É um não querer mais que bem querer;\r\n" + 
-				"É solitário andar por entre a gente;\r\n" + 
-				"É nunca contentar-se de contente;\r\n" + 
-				"É cuidar que se ganha em se perder;\r\n" + 
-				"\r\n" + 
-				"É querer estar preso por vontade;\r\n" + 
-				"É servir a quem vence, o vencedor;\r\n" + 
-				"É ter com quem nos mata lealdade.\r\n" + 
-				"\r\n" + 
-				"Mas como causar pode seu favor\r\n" + 
-				"Nos corações humanos amizade,\r\n" + 
-				"Se tão contrário a si é o mesmo Amor?");
-//		Shingle.writeShingleFile("C:\\Users\\Ahri Gonçalves\\Desktop");
-		
+		Shingle.createShingleFile("C:\\Users\\Ahri Gonçalves\\eclipse-workspace\\p3\\src\\mpei\\test.txt"); // pode ser qualquer path
 	}
 }
