a9cc4082ffe59c97bad510a80dff852119706817
Ariana Gonçalves <arygoncalves15@gmail.com>
2019-12-02 18:53:15 +0000

    Creates shingle file and matrix for minhash

--- Shingle.java
+++ Shingle.java
@@ -5,13 +5,17 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Shingle {
 	private static int ktitle = 4;
 	private static int kcontent = 10;
-	
+	private static int numhash = 10;
+	public static int matrixTitle[][] = new int[50][numhash];
+	public static int matrixContent[][] = new int[50][numhash];
+	private static int counter = 0;
 	
 	public static void createShingleFile(String path){
 		File fx = new File(path);
@@ -27,10 +31,10 @@ public class Shingle {
 		            line = in.readLine();
 	            }
 	            if(line.startsWith("*** START OF THIS PROJECT GUTENBERG EBOOK")) {
-	            	do {
+	            	while(!((line = in.readLine()).startsWith("*** END OF THIS PROJECT GUTENBERG EBOOK"))) {
 	            		line = in.readLine();
 	            		sb.append(line);
-	            	}while(!((line = in.readLine()).startsWith("*** END OF THIS PROJECT GUTENBERG EBOOK")));
+	            	}
 	            }
 	        }
 	        content = sb.toString();
@@ -48,29 +52,23 @@ public class Shingle {
 	}
 			
 	private static String[] createShingleTitle(String titulo) {
-		String s = titulo.replaceAll("[^a-zA-Z0-9]", "");
-		List<String> shingles = new ArrayList<>();
+		String s = titulo.replaceAll("[^a-zA-Z0-9 ]", "");
+		Set<String> shingles = new HashSet<>();
 		for(int j=0; j<s.length()-ktitle+1; j++) {
 			String word = "";
 			word = s.substring(j, j+ktitle).toLowerCase();
-			if(!shingles.contains(word)) {
-				shingles.add(word);
-				//System.out.print(word + " ");
-			}
+			shingles.add(word);
 		}
 		return shingles.toArray(new String[0]);
 	}
 	
 	private static String[] createShingleContent(String content){
-		String s = content.replaceAll("[^a-zA-Z0-9]", "");
-		List<String> shingles = new ArrayList<>();
+		String s = content.replaceAll("[^a-zA-Z0-9 ]", "");
+		Set<String> shingles = new HashSet<>();
 		for(int j=0; j<s.length()-kcontent+1; j++) {
 			String word = "";
 			word = s.substring(j, j+kcontent).toLowerCase();
-			if(!shingles.contains(word)) {
-				shingles.add(word);
-				//System.out.println(word + " ");
-			}
+			shingles.add(word);
 		}
 		return shingles.toArray(new String[0]);
 	}
@@ -79,21 +77,74 @@ public class Shingle {
 		File fx = new File("C:\\Users\\Ahri Gonçalves\\eclipse-workspace\\p3\\src\\mpei\\" + title + "_shingles.txt"); 
 		PrintWriter pw = new PrintWriter(fx);
 		for(String i: titleset)
-			pw.print(i + " ");
+			pw.print(i + "_");					//shingles divididos por _
 		pw.println("");
-		pw.println("");
+		pw.println("***");
 		for(String i: contentset)
-			pw.print(i + " ");
+			pw.print(i + "_");					//shingles divididos por _
 		pw.close();
-		System.out.print("SHINGLE FILE FOR "+title+" BOOK CREATED!");
+		System.out.println("SHINGLE FILE FOR "+title+" BOOK CREATED!");
 	}
 	
-	// adicional para hashing dos shingles, diminuir o tamanho
-//	private static long shingleToHash(String[] contentset) {
-//		
-//	}
+	//hashing dos shingles
+	private static int shingleToHash(String shingle) {
+		int hash = 0;
+		for (int i = 0; i < shingle.length(); i++) {
+			hash = shingle.charAt(i) + ((hash << 5) - hash);
+		}
+		return hash;
+	}
+	
+	public static void readShingleFile(String path) throws IOException {
+		counter++;
+		File fx = new File(path);
+		Scanner read = new Scanner(fx);
+		while(read.hasNextLine()) {
+			String line = read.nextLine();
+			if(line.equals("***")) {
+				break;
+			}
+			Scanner readtitle = new Scanner(line).useDelimiter("_");
+			while(readtitle.hasNext()) {
+				String shingle = readtitle.next();
+				HashFunction[] hf = getHashFunctions(shingle);
+				for(int i=0; i<hf.length; i++) {
+					if(matrixTitle[counter][i] == 0 || matrixTitle[counter][i] > hf[i].signature()) {
+						matrixTitle[counter][i] = hf[i].signature();
+					}
+				}
+			}
+		}
+		while(read.hasNextLine()) {
+			String line = read.nextLine();
+			Scanner readcontent = new Scanner(line).useDelimiter("_");
+			while(readcontent.hasNext()){
+				String shingle = readcontent.next();
+				HashFunction[] hf = getHashFunctions(shingle);
+				for(int i=0; i<hf.length; i++) {
+					if(matrixContent[counter][i] == 0 || matrixContent[counter][i] > hf[i].signature()) {
+						matrixContent[counter][i] = hf[i].signature();
+					}
+				}
+			}
+		}
+	}
+	
+	private static HashFunction[] getHashFunctions(String shingle) {
+		int id = shingleToHash(shingle);
+		HashFunction array[] = new HashFunction[numhash];
+		for(int i=0; i<array.length; i++) {
+			array[i] = new HashFunction(id);
+		}
+		return array;
+	}
 	
 	public static void main(String args[]) throws IOException {
 		Shingle.createShingleFile("C:\\Users\\Ahri Gonçalves\\eclipse-workspace\\p3\\src\\mpei\\test.txt"); // pode ser qualquer path
+		Shingle.readShingleFile("C:\\Users\\Ahri Gonçalves\\eclipse-workspace\\p3\\src\\mpei\\Dracula_shingles.txt");
+//		for (int[] row : matrixTitle) 
+//            System.out.println(Arrays.toString(row)); 
+//		for (int[] row : matrixContent) 
+//            System.out.println(Arrays.toString(row)); 
 	}
 }
