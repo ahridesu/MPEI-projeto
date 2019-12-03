f801349002a547124e1d98cc38210ea44b0adf06
Ariana Gonçalves <arygoncalves15@gmail.com>
2019-12-03 15:22:38 +0000

    Lista/Set de livros

---
+++ ListaLivros.java
@@ -0,0 +1,33 @@
+package mpei;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaLivros {
+	
+	public static Set<Livro> lista = new HashSet<>();			// set de livros, que contem o indice do livro, titulo e se está requisitado
+	
+	public Livro[] getLista() {
+		return lista.toArray(new Livro[0]);
+	}
+	
+	public static int getIndex(String titulo) {
+		if(exists(titulo)) {
+			for(Livro l: lista) {
+				if(l.getTitulo().equals(titulo)) {
+					return l.getIndice();
+				}
+			}
+		}					
+		return -1;				// nao existe
+	}
+	
+	private static boolean exists(String titulo) {
+		for(Livro l: lista) {
+			if(l.getTitulo().equals(titulo)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
